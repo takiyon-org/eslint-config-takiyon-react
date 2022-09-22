5f21685e5e7c0fd81826e755d6107f3f0c4cd97b
Jake Zatecky <jake.zatecky@gmail.com>
2022-09-22 11:28:02 -0400

    Add simple tests

---
+++ test/ComponentTest.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class ComponentTest extends React.Component {
+    // react/sort-prop-types'
+    static propTypes = {
+        label: PropTypes.node.isRequired,
+        options: PropTypes.arrayOf(PropTypes.shape({
+            label: PropTypes.node.isRequired,
+            value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+        })).isRequired,
+
+        disabled: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        disabled: false,
+    };
+
+    state = { value: [] };
+
+    constructor(props) {
+        super(props);
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(value) {
+        this.setState({ value });
+    }
+
+    render() {
+        const { label, options, disabled } = this.props;
+        const { value } = this.state;
+
+        // jsx-a11y/label-has-associated-control
+        // react/jsx-sort-props
+        return (
+            <label>
+                {label}
+                <select
+                    disabled={disabled}
+                    value={value}
+                    onChange={this.onChange}
+                >
+                    {options.forEach((option) => (
+                        <option value={option.label}>
+                            {option.value}
+                        </option>
+                    ))}
+                </select>
+            </label>
+        );
+    }
+}
+
+export default ComponentTest;
