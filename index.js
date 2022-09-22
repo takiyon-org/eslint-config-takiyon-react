a5e9e941d652ae5deb07845bc53952ba71caee53
Jake Zatecky <jake.zatecky@gmail.com>
2022-09-22 11:02:15 -0400

    Add explanations for each rule
    Also re-allow enforcement of `label` requirements

--- index.js
+++ index.js
@@ -4,24 +4,36 @@ module.exports = {
         'takiyon',
     ],
     rules: {
-        'jsx-a11y/label-has-for': 'off',
+        // Allow `.js` files to have JSX
         'react/jsx-filename-extension': 'off',
+
+        // Two spaces are not enough for readability
         'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props' : ['error', 4],
+
+        // Allow spreading on HTML elements in wrapper components
         'react/jsx-props-no-spreading': ['error', {
             html: 'ignore',
             custom: 'enforce',
             exceptions: [],
         }],
+
+        // Alphabetical sort of props, with `key` first and callbacks last
         'react/jsx-sort-props': ['error', {
             callbacksLast: true,
             reservedFirst: ['key'],
         }],
+
+        // Alphabetical sort of prop types, with required props first
         'react/sort-prop-types': ['error', {
             callbacksLast: true,
             requiredFirst: true,
         }],
+
+        // Allow author initialize state in whatever manner they prefer
         'react/state-in-constructor': 'off',
+
+        // Require static fields in React to be static methods in class
         'react/static-property-placement': ['error', 'static public field'],
     },
 };
